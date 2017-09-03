bfed44de58154ffd9499648befff14dcc6696a6c
hanfengzqh <1017459102@qq.com>
2017-09-03 18:05:24 +0800

    jdk 8 learn

--- app/src/main/java/com/zqh/jdk/MainActivity.java
+++ app/src/main/java/com/zqh/jdk/MainActivity.java
@@ -1,7 +1,9 @@
 package com.zqh.jdk;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.bt_button);
+        button.setOnClickListener((v) -> {
+            System.out.println("onclick--jdk 8 新特性");
+            Toast.makeText(getApplicationContext(),"点击",Toast.LENGTH_SHORT).show();
+        });
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//
+//            }
+//        });
     }
 }
